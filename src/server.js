75d7b7e367e760457d569e9120183fee00e45b00
candle <ragzouken@gmail.com>
2020-12-20 21:08:12 +0000

    saving

--- src/server.js
+++ src/server.js
@@ -7,15 +7,33 @@ const fileUpload = require('express-fileupload');
 
 require('dotenv').config();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync');
+const db = low(new FileSync(process.env.DATA_PATH, { serialize: JSON.stringify, deserialize: JSON.parse }));
+
 process.title = "zone library";
 mkdir(process.env.MEDIA_PATH, () => {});
 
-const library = new Map();
+db.defaults({
+    entries: [],
+}).write();
+
+const library = new Map(db.get("entries"));
 library.set("yoda", {
+    id: "yoda",
     title: "yoda spins",
     media: "/",
     duration: "400s",
 });
+
+function save() {
+    db.set("entries", Array.from(library)).write();
+}
+
+async function getMediaDurationInSeconds(file) {
+    const info = await ffprobe(file, { path: ffprobeStatic.path });
+    return info.streams[0].duration;
+}
 
 const app = express();
 app.use(fileUpload({
@@ -24,7 +42,8 @@ app.use(fileUpload({
     limits: { fileSize: 16 * 1024 * 1024 },
 }));
 
-process.on('SIGINT', function() {
+process.on('SIGINT', () => {
+    save();
     process.exit();
 });
 
@@ -32,7 +51,7 @@ app.use(express.static("public"));
 app.use("/media", express.static("media"));
 
 app.get("/library", (request, response) => {
-    response.json(request.query.q);
+    response.json(Array.from(library.values()));
 });
 
 app.get("/library/:id", (request, response) => {
@@ -52,15 +71,18 @@ app.post("/library", async (request, response) => {
     const path = `${process.env.MEDIA_PATH}/${id}${type}`;
 
     await file.mv(path);
+    const duration = await getMediaDurationInSeconds(path).catch(() => 0);
     
     const info = {
         id,
         title: request.body.title,
         src: path,
+        duration,
     }
     
     library.set(id, info);
     response.json(info);
+    save();
 });
 
 app.put("/library/:id", (request, response) => {
